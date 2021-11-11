aa78f2215bd22313021cd95862088570a4a69e1d
elkabaskal <audiopt@list.ru>
2021-11-11 12:18:53 +0500

    task 1 for f_p is completed

--- first_project/src/views/Home.vue
+++ first_project/src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Calculator" />
+    <div class="display">
+      <input
+        type="number"
+        placeholder="Input operand1"
+        v-model.number="operand1"
+      />
+      <input
+        type="number"
+        placeholder="Input operand2"
+        v-model.number="operand2"
+      />
+      <br />
+      Result: {{ result }}
+    </div>
+    <div class="keyboard">
+      <button @click="onPlus">+</button>
+      <button @click="onMinus">-</button>
+      <button :disabled="operand2 == 0" @click="onDiv">/</button>
+      <button @click="onMul">*</button>
+      <button @click="onElevate">Elevate</button>
+      <button @click="onRemainder">Remainder</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Calculator",
+  data() {
+    return {
+      operand1: 0,
+      operand2: 0,
+      result: 0,
+    };
+  },
+  methods: {
+    onPlus() {
+      this.result = this.operand1 + this.operand2;
+    },
+    onMinus() {
+      this.result = this.operand1 - this.operand2;
+    },
+
+    onMul() {
+      this.result = this.operand1 * this.operand2;
+    },
+
+    onDiv() {
+      this.result = this.operand1 / this.operand2;
+    },
+    onElevate() {
+      this.result = this.operand1 ** this.operand2;
+    },
+    onRemainder() {
+      this.result = this.operand1 % this.operand2;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
