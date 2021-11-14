19f91a02c6912d7459e3e44c6a80dc739414c82a
elkabaskal <audiopt@list.ru>
2021-11-14 22:02:50 +0500

    task for lesson 2 is ready

--- first_project/src/views/Home.vue
+++ first_project/src/views/Home.vue
@@ -5,24 +5,43 @@
     <div class="display">
       <input
         type="number"
-        placeholder="Input operand1"
+        placeholder="Input Operand 1"
         v-model.number="operand1"
       />
       <input
         type="number"
-        placeholder="Input operand2"
+        placeholder="Input Operand 2"
         v-model.number="operand2"
       />
       <br />
-      Result: {{ result }}
+      <span>Result: {{ result }}</span>
     </div>
     <div class="keyboard">
-      <button @click="onPlus">+</button>
-      <button @click="onMinus">-</button>
-      <button :disabled="operand2 == 0" @click="onDiv">/</button>
-      <button @click="onMul">*</button>
-      <button @click="onElevate">Elevate</button>
-      <button @click="onRemainder">Remainder</button>
+      <button
+        v-for="operand in operands"
+        v-bind:key="operand"
+        v-bind:disabled="operand1 === '' || operand2 === ''"
+        @click="calculate(operand)"
+      >
+        {{ operand }}
+      </button>
+      <br />
+      <input type="checkbox" id="checkbox" v-model="checked" />
+      <span>Show keyboard</span>
+      <div class="keyboard" v-show="checked">
+        <button
+          v-for="number in numbers"
+          v-bind:key="number"
+          @click="numberInput(number)"
+        >
+          {{ number }}
+        </button>
+        <br />
+        <input type="radio" name="field" id="one" value="one" v-model="field" />
+        <label for="one">Operand 1</label>
+        <input type="radio" name="field" id="two" value="two" v-model="field" />
+        <label for="two">Operand 2</label>
+      </div>
     </div>
   </div>
 </template>
@@ -35,15 +54,20 @@ export default {
   name: "Calculator",
   data() {
     return {
-      operand1: 0,
-      operand2: 0,
+      operand1: "",
+      operand2: "",
       result: 0,
+      operands: ["+", "-", "/", "*", "**", "%", "C"],
+      numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "Back"],
+      checked: false,
+      field: "one",
     };
   },
   methods: {
     onPlus() {
       this.result = this.operand1 + this.operand2;
     },
+
     onMinus() {
       this.result = this.operand1 - this.operand2;
     },
@@ -53,13 +77,73 @@ export default {
     },
 
     onDiv() {
-      this.result = this.operand1 / this.operand2;
+      if (this.operand2 === 0) {
+        this.result = "You cannot divide by 0 !!!";
+      } else {
+        this.result = this.operand1 / this.operand2;
+      }
     },
+
     onElevate() {
       this.result = this.operand1 ** this.operand2;
     },
+
     onRemainder() {
-      this.result = this.operand1 % this.operand2;
+      if (this.operand2 === 0) {
+        this.result = "You cannot divide by 0 !!!";
+      } else {
+        this.result = this.operand1 % this.operand2;
+      }
+    },
+
+    onClear() {
+      this.operand1 = "";
+      this.operand2 = "";
+      this.result = 0;
+    },
+
+    calculate(operation = "+") {
+      this.operand1 = Number(this.operand1);
+      this.operand2 = Number(this.operand2);
+      switch (operation) {
+        case "+":
+          this.onPlus();
+          break;
+        case "-":
+          this.onMinus();
+          break;
+        case "*":
+          this.onMul();
+          break;
+        case "/":
+          this.onDiv();
+          break;
+        case "**":
+          this.onElevate();
+          break;
+        case "%":
+          this.onRemainder();
+          break;
+        case "C":
+          this.onClear();
+          break;
+      }
+    },
+
+    numberInput(addNumber) {
+      if (this.field === "one") {
+        if (addNumber === "Back") {
+          this.operand1 = String(this.operand1);
+          return (this.operand1 = this.operand1.slice(0, -1));
+        }
+        this.operand1 = `${this.operand1}${addNumber}`;
+      } else {
+        if (addNumber === "Back") {
+          this.operand2 = String(this.operand2);
+          return (this.operand2 = this.operand2.slice(0, -1));
+        }
+        this.operand2 = `${this.operand2}${addNumber}`;
+      }
     },
   },
   components: {
